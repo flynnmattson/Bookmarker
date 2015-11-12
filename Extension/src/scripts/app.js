c1f0f48c0c86f18f39a9d0463f39b8b876596db1
renanpeixotox <renanpeixotox@gmail.com>
2015-11-12 06:58:18 -0800

    Login, HomeScreen & Project - Updates
     Created different folders for the Website and the Extension
    Fixed - Keep user signed in
    Added Search bar to the homescreen
    Added footer to the homescreen
    Added some styles to ./css/general.css
    Added folder ./fonts/ - works with bootstrap
    Added Bootstrap glyphs to the Accordion Panels in the homescreen (little icons) - the code changed I little bit
    Commented ./scripts/app.js (the code I wrote) to help in if/when it gets big
    Users can signed out too!

--- Extension/src/scripts/app.js
+++ Extension/src/scripts/app.js
@@ -1,58 +1,63 @@
 var app = angular.module('Bookmarker', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'firebase']);
+
 app.factory('Auth', ['$firebaseAuth',
   function($firebaseAuth) {
     var ref = new Firebase('https://de-bookmarker.firebaseio.com/');
     return $firebaseAuth(ref);
   }
 ]);
+
 app.factory('ref', [
   function() {
     return new Firebase('https://de-bookmarker.firebaseio.com/');
   }
 ]);
 
-app.controller('LoginCtrl', ['$scope', 'Auth',
-    function($scope, Auth){
+app.controller('LoginCtrl', ['$scope', 'Auth', 'ref',
+    function($scope, Auth, ref) {
 
+    // close an specified alert message
     $scope.closeAlert = function(index) {
       $scope.alerts.splice(index, 1);
     };
 
     $scope.Login = function() {
-
+      // clear all alert messages
       $scope.alerts = [];
-      var allComplete = $scope.email && $scope.password;
 
+      // verify if all fields have been filled in
+      var allComplete = $scope.email && $scope.password;
       if(!allComplete)
         $scope.alerts.push({type: 'danger', msg: 'Please enter your email and password!'});
+
       else {
 
+        // try to authenticate user
         Auth.$authWithPassword({
           email : $scope.email,
           password : $scope.password
 
         }).then(function(authData) {
-          console.log("Logged in as:", authData.uid);
-          $scope.alerts.push({type: 'success', msg: 'You are logged in!'});
+          // if successfull, user has been signed in to his/her account
+          // store the Firebase authentication token for this session
+          chrome.storage.local.set({'AUTH_TOKEN': authData.token});
+          // redirect user to the home screen
           window.location.href = './home-screen.html';
-        }).catch(function(error) {
 
+        }).catch(function(error) {
+          // user could not be logged in
           switch (error.code) {
             case "INVALID_EMAIL":
-              console.log(error.message);
               $scope.alerts.push({type: 'danger', msg: 'The email address you entered is not a valid email.'});
               break;
             case "INVALID_USER":
-              console.log(error.message);
               $scope.alerts.push({type: 'danger', msg: 'The specified user account does not exist.'});
               break;
             case "INVALID_PASSWORD":
-              console.log(error.message);
               $scope.alerts.push({type: 'danger', msg: 'The password you entered is incorrect.'});
             break;
             default:
-              console.log(error.message);
-              $scope.alerts.push({type: 'danger', msg: 'There was an error signing you in to your account:' + error.message});
+              $scope.alerts.push({type: 'danger', msg: 'There was an error signing you in to your account: ' + error.message});
           }
         });
       }
@@ -63,51 +68,66 @@ app.controller('LoginCtrl', ['$scope', 'Auth',
 app.controller('RegisterCtrl', ['$scope', 'Auth',
     function($scope, Auth){
 
+    // close an specified alert message
     $scope.closeAlert = function(index) {
       $scope.alerts.splice(index, 1);
     };
 
+    // redirect user back to the login page
     $scope.cancel = function() {
       window.location.href = './login.html';
     };
 
     $scope.createAccount = function() {
 
+      // clear all alert messages
       $scope.alerts = [];
-      var allComplete = $scope.name && $scope.email && $scope.password;
 
+      // verify if all fields have been filled in
+      var allComplete = $scope.name && $scope.email && $scope.password && $scope.repassword;
       if(!allComplete)
         $scope.alerts.push({type: 'danger', msg: 'Please complete all fields!'});
+
       else {
 
-        var regExpName = new RegExp(/^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/);
+        var error = false;
 
+        // verify if passwords match
+        if($scope.password != $scope.repassword){
+          $scope.alerts.push({type: 'danger', msg: 'The password does not match confirmation!'});
+          error = true;
+        }
+
+        // verify if the name user has entered is a valid name
+        var regExpName = new RegExp(/^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/);
         if(!regExpName.test($scope.name)) {
           $scope.alerts.push({type: 'danger', msg: 'The name you entered is not a valid name.'});
           $scope.alerts.push({type: 'warning', msg: 'Please enter your first and last name.'});
-        } else {
+          error = true;
+        }
 
+        if(!error) {
+
+          // try to register a new user
           Auth.$createUser({
             email : $scope.email,
             password : $scope.password
 
           }).then(function(userData) {
-            console.log('User created with uid: ' + userData.uid);
-            $scope.alerts.push({type: 'success', msg: 'Your account has been created!'});
+            // if successfull, a new user account has been created
+            alert('Your account has been created!');
+            window.location.href = './login.html';
 
           }).catch(function(error) {
-
+            // the new user account could not be created
             switch (error.code) {
               case "EMAIL_TAKEN":
-                console.log(error.message);
                 $scope.alerts.push({type: 'danger', msg: 'The email address you entered is already in use.'});
                 break;
               case "INVALID_EMAIL":
-                console.log(error.message);
                 $scope.alerts.push({type: 'danger', msg: 'The email address you entered is not a valid email.'});
                 break;
               default:
-                console.log(error.message);
                 $scope.alerts.push({type: 'danger', msg: 'There was an error creating your account:' + error.message});
             }
           });
@@ -117,9 +137,47 @@ app.controller('RegisterCtrl', ['$scope', 'Auth',
   }
 ]);
 
-app.controller('AccordionDemoCtrl', ['$scope', 'ref',
-    function ($scope, ref) {
-      $scope.oneAtATime = false;
+app.controller('HomeScreenCtrl', ['$scope', 'Auth', 'ref',
+    function($scope, Auth, ref) {
+
+      // try to retrieve token from local storage
+      chrome.storage.local.get('AUTH_TOKEN', function (result) {
+        if(result.AUTH_TOKEN){
+          // try to authenticate user again with the token stored in the last session
+          Auth.$authWithCustomToken(
+            result.AUTH_TOKEN
+
+          ).then(function(authData) {
+            // if successfull, user has been signed in to his/her account
+          }).catch(function(error) {
+            // user could not be logged in
+            window.location.href = './login.html';
+          });
+        }
+        else {
+          // user has not logged in yet
+          window.location.href = './login.html';
+        }
+      });
+
+      // unauthenticate user and remove token from local storage
+      $scope.logout = function() {
+        // sign out user
+        ref.unauth();
+        var authData = ref.getAuth();
+        if(authData) {
+          // user could not be logged out
+        }
+        else {
+          // if nothing, user has been signed out
+          // remove token from local storage
+          chrome.storage.local.remove('AUTH_TOKEN', function(){
+              alert('You have successfully signed out!');
+              window.location.href = './login.html';
+          });
+        }
+      };
+
 
       // Gives you the URL of the current tab on the browser.
       chrome.tabs.getSelected(null, function(tab){
@@ -132,11 +190,6 @@ app.controller('AccordionDemoCtrl', ['$scope', 'ref',
       };
 
       $scope.items = [];
-
-      $scope.status = {
-        isFirstOpen: true,
-        isFirstDisabled: false
-      };
 
       $scope.linkBookmarks = function() {
         chrome.bookmarks.getTree(function(itemTree){
