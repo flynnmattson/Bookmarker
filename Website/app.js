8cb36af60447b3b797badaf6003543a6d916ccc4
Dalia Swellum <Dalia@D-10-156-10-88.dhcp4.washington.edu>
2015-11-19 19:43:38 -0800

    Website Updates
    flower pic, links, etc.

---
+++ Website/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('Bookmarker', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'firebase']);
+
+app.factory('Auth', ['$firebaseAuth',
+  function($firebaseAuth) {
+    var ref = new Firebase('https://de-bookmarker.firebaseio.com/');
+    return $firebaseAuth(ref);
+  }
+]);
+
+app.factory('ref', [
+  function() {
+    return new Firebase('https://de-bookmarker.firebaseio.com/');
+  }
+]);
