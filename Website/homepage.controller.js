32bcb3dfa047c6f5659c61d99b0e1edf1b07fd53
nawahidi <wahidi.nabiha@gmail.com>
2015-11-25 00:58:31 -0800

    Download Button  creation
    Attempt to link Download button to the chrome store.
    Angular accordian for FAQ issue (already discussed)

---
+++ Website/homepage.controller.js
@@ -0,0 +1,10 @@
+app.controller('homepageCtrl', '$scope', 'Auth', 'ref',
+    function($scope, Auth, ref) {
+
+      $scope.downloadExtension = function()
+      {
+          window.location.href = url('https://chrome.google.com/webstore/category/apps';
+      }
+
+  }
+]);
